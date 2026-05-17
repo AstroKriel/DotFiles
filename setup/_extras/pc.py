4be1dd299511f1aee2ad4d1aab6c01c9b0304abc
Neco Kriel <necokriel@gmail.com>
2026-05-17 14:13:27 +0200

    rename(personal-machine/): rename to pc/; update module and source paths.

--- setup/_extras/pc.py
+++ setup/_extras/pc.py
@@ -4,10 +4,10 @@ from pathlib import Path
 from setup._extras.config import ExtraConfig, EXTRAS_DIR
 
 EXTRAS: dict[str, ExtraConfig] = {
-    "personal-machine/path-aliases.sh":
+    "pc/path-aliases.sh":
     ExtraConfig(
-        name="personal machine path aliases",
-        source_path=EXTRAS_DIR / "personal-machine" / "path-aliases.sh",
+        name="pc path aliases",
+        source_path=EXTRAS_DIR / "pc" / "path-aliases.sh",
         target_path=Path.home() / ".path_aliases",
     ),
 }
